78fe005262095ea6dea2c6fc03c397b3f0f27c2c
chen xiaoli <34932601+Chen-Xiaoli@users.noreply.github.com>
2025-07-18 11:02:43 +0800

    Time: 4 ms (18.08%) | Memory: 18 MB (30.58%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,12 +1,31 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
 
-        length = len(prices)
+        # length = len(prices)
 
         
-        for i in range(length):
-            for j in range(i+1, length):
-                if prices[j] <= prices[i]:
-                    prices[i] = prices[i] -  prices[j]
-                    break
-        return prices
+        # for i in range(length):
+        #     for j in range(i+1, length):
+        #         if prices[j] <= prices[i]:
+        #             prices[i] = prices[i] -  prices[j]
+        #             break
+        # return prices
+
+        n = len(prices)
+        ans = [0] * n
+        stack = []
+
+        for i in range(n-1, -1, -1):
+            p = prices[i]
+
+            while stack and stack[-1] > p:
+                stack.pop()
+            
+            if stack:
+                ans[i] = p - stack[-1]
+            else:
+                ans[i] = p
+            
+            stack.append(p)
+        return ans
+
