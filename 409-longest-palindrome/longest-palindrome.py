4839da34b960ae0f8d801bc58c676b060e61a088
chen xiaoli <34932601+Chen-Xiaoli@users.noreply.github.com>
2025-07-21 13:40:35 +0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (45.82%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -3,21 +3,13 @@ class Solution:
         cnt = Counter(s)
 
         length = 0
-        odd = 0
-        even = 0
 
         for key, value in cnt.items():
             if value%2 == 0:
-                odd += value/2
+                length += value
             else:
-                if value > 1:
-                    odd += math.floor(value/2)
-                even += 1
+                length += value - 1
+                cnt[key] = 1
         
-        if even:
-            length = int(odd * 2 + 1)
-        else:
-            length = int(odd * 2)
-
-        return length 
+        return length + 1 if 1 in cnt.values() else length
         
