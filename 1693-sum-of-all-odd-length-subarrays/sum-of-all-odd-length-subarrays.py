45baa43896b27436912d4197f99e98d70cc55101
chen xiaoli <34932601+Chen-Xiaoli@users.noreply.github.com>
2025-07-21 14:29:00 +0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.26%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
@@ -1,19 +1,24 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        ans = 0
+        # ans = 0
 
-        preSum = [0] * (len(arr) + 1)
+        # preSum = [0] * (len(arr) + 1)
 
-        for i in range(len(arr)):
-            preSum[i+1] = preSum[i] + arr[i]
-
-        print('preSum:', preSum)
+        # for i in range(len(arr)):
+        #     preSum[i+1] = preSum[i] + arr[i]
         
-        for i in range(len(arr)):
-            for j in range(1, len(arr)+1, 2):
-                if i+j-1 < len(arr):
-                    ans += preSum[i + j] - preSum[i]
+        # for i in range(len(arr)):
+        #     for j in range(1, len(arr)+1, 2):
+        #         if i+j-1 < len(arr):
+        #             ans += preSum[i + j] - preSum[i]
                     
+        # return ans
+
+        n = len(arr)
+        ans = 0
+        for i, num in enumerate(arr):
+            total = ((i + 1) * (n - i) + 1) // 2
+            ans += num * total
         return ans
 
 
