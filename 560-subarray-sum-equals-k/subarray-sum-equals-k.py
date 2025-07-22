26819f6c3bde9e1ec68a4a16f9244bbfd4193b6c
chen xiaoli <34932601+Chen-Xiaoli@users.noreply.github.com>
2025-07-22 11:15:24 +0800

    Time: 47 ms (9.74%) | Memory: 22.2 MB (5.23%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,43 +1,43 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # count = 0
+        count = 0
 
-        # pre = 0
+        pre = 0
 
-        # mp = collections.defaultdict(int)
+        mp = collections.defaultdict(int)
 
-        # mp[0] = 1
-
-        # for i in range(len(nums)):
-        #     pre += nums[i]
-
-        #     count += mp[pre-k]
-        #     mp[pre] += 1
-        
-        # return count
-
-        preSum = [0] * (len(nums) + 1)
+        mp[0] = 1
 
         for i in range(len(nums)):
-            preSum[i+1] = preSum[i] + nums[i]
+            pre += nums[i]
+
+            count += mp[pre-k]
+            mp[pre] += 1
         
-        dic = {}
+        return count
 
-        ans = 0
+        # preSum = [0] * (len(nums) + 1)
 
-        for i in range(len(preSum)):
+        # for i in range(len(nums)):
+        #     preSum[i+1] = preSum[i] + nums[i]
+        
+        # dic = {}
+
+        # ans = 0
+
+        # for i in range(len(preSum)):
            
-            if preSum[i] - k in dic:
-                ans += dic[preSum[i] - k]
-            else:
-                dic[preSum[i] - k] = 0
+        #     if preSum[i] - k in dic:
+        #         ans += dic[preSum[i] - k]
+        #     else:
+        #         dic[preSum[i] - k] = 0
             
-            if preSum[i] in dic:
-                dic[preSum[i]] += 1
-            else:
-                 dic[preSum[i]] = 1
+        #     if preSum[i] in dic:
+        #         dic[preSum[i]] += 1
+        #     else:
+        #          dic[preSum[i]] = 1
           
-        return ans
+        # return ans
 
             
 
