0267474a264737046e1ba9c3a817ae255eb9304f
chen xiaoli <34932601+Chen-Xiaoli@users.noreply.github.com>
2025-07-24 11:48:21 +0800

    Time: 29 ms (36.86%) | Memory: 23.3 MB (79.71%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,21 +1,30 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        ans = []
-        preProduct = nums[0]
-        total = nums[0]
-        dic = {}
-        left = [1] * len(nums)
-        right = [1] * len(nums)
+        ans = [1] * len(nums)
+      
+        # left = [1] * len(nums)
+        # right = [1] * len(nums)
+
+        # for i in range(1, len(nums)):
+        #     left[i] = left[i-1] * nums[i-1]
+
+        # for i in range(len(nums)-2, -1, -1):
+        #     right[i] = right[i+1] * nums[i+1]
+
+        # for i in range(len(nums)):
+        #     ans.append(left[i] * right[i]) 
 
         for i in range(1, len(nums)):
-            left[i] = left[i-1] * nums[i-1]
+            ans[i] = ans[i-1] * nums[i-1]
 
-        for i in range(len(nums)-2, -1, -1):
-            right[i] = right[i+1] * nums[i+1]
+        right = 1
 
-        for i in range(len(nums)):
-            ans.append(left[i] * right[i]) 
+        for i in range(len(nums)-1, -1, -1):
+
+            ans[i] = ans[i] * right
+            right *= nums[i]
+
         return ans
             
 
